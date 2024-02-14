0dbc0b338ff483c5a14e8717bfc500f21cbb06c8
luca-macedone <luca.macedone@outlook.it>
2024-02-14 17:58:40 +0100

    init

--- src/screen/dashboard/MenuView.js
+++ src/screen/dashboard/MenuView.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import LoadingComp from "../../components/LoadingComp";
 
 const MenuView = () => {
-  return <div>MenuView</div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const baseUrl =
+        "https://65c3642539055e7482c0c4ba.mockapi.io/api/v1/Restaurant";
+      await axios
+        .get(baseUrl)
+        .then((response) => {
+          console.log(response.data);
+          setRestaurants(response.data);
+        })
+        .catch((err) => console.error(err))
+        .finally(() => {
+          setIsLoading(false);
+        });
+    };
+    setIsLoading(true);
+    fetchData();
+  }, []);
+  return (
+    <div className="grid grid-flow-row grid-cols-1 lg:grid-cols-2">
+      <div>
+        <h2>Your Restaurants</h2>
+        <div>
+          {isLoading ? (
+            <LoadingComp />
+          ) : (
+            restaurants.map((rest) => {
+              return <div key={rest.id}>{rest.name}</div>;
+            })
+          )}
+        </div>
+      </div>
+      <div>menu of the restaurant</div>
+    </div>
+  );
 };
 
 export default MenuView;
