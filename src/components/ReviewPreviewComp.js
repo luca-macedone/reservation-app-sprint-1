341d6b19d9dd67402e7c35a6732bedb64af66416
luca-macedone <luca.macedone@outlook.it>
2024-02-14 16:02:15 +0100

    add reviews preview inside condesed dashboard

--- src/components/ReviewPreviewComp.js
+++ src/components/ReviewPreviewComp.js
@@ -1,7 +1,59 @@
+import {
+  faStar as faStarSolid,
+  faStarHalfStroke,
+} from "@fortawesome/free-solid-svg-icons";
+import { faStar as faStarRegular } from "@fortawesome/free-regular-svg-icons";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const ReviewPreviewComp = () => {
-  return <div>ReviewPreviewComp</div>;
+const ReviewPreviewComp = (data) => {
+  const { name, rating, id } = data.data;
+  const formattedRating = (_rating) => {
+    // const maxStars = 10;
+    let stars = [];
+
+    for (let i = 1; i <= 100; i += 10) {
+      if (i < _rating && i + 1 > _rating) {
+        // console.log(i, " v ", _rating, "half");
+        stars.push(
+          <FontAwesomeIcon
+            key={i}
+            icon={faStarHalfStroke}
+            className="me-1 text-accent"
+          />
+        );
+      } else if (i <= _rating) {
+        // console.log(i, " v ", _rating, "full");
+        stars.push(
+          <FontAwesomeIcon
+            key={i}
+            icon={faStarSolid}
+            className="me-1 text-accent"
+          />
+        );
+      } else {
+        // console.log(i, " v ", _rating, "empty");
+        stars.push(
+          <FontAwesomeIcon
+            key={i}
+            icon={faStarRegular}
+            className="me-1 text-accent"
+          />
+        );
+      }
+    }
+
+    return stars;
+  };
+  return (
+    <div
+      className="bg-light rounded-xl px-5 py-3 w-full flex items-center justify-between ring-2 ring-transparent hover:ring-accent hover:cursor-pointer text-dark hover:text-accent transition-all ease-in-out duration-200"
+      key={id}
+    >
+      <h5 className="font-bold capitalize">{name}</h5>
+      <div>{formattedRating(rating)}</div>
+    </div>
+  );
 };
 
 export default ReviewPreviewComp;
